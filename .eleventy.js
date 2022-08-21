2b587d8e9b4b586329e31eeea5a3a3680da150dc
Glitch (glitch-hello-eleventy) <none>
2022-08-21 18:22:52 +0000

    😎🍂 Checkpoint ./.eleventy.js:43931460/664

--- .eleventy.js
+++ .eleventy.js
@@ -44,19 +44,11 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
 
-  /* Build the collection of posts to list in the site
-     - Read the Next Steps post to learn how to extend this
-  */
   eleventyConfig.addCollection("posts", function(collection) {
-    
-    /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
-       - https://www.11ty.dev/docs/collections/
-    */
-    
-    // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
-    // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
+
     const coll = collection
-      .getFilteredByTag("posts");
+      .getFilteredByTag("posts")
+      .sort((a, b) => b.data.date - a.data.date);
 
     // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
     // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
