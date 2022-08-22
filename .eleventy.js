9939881ca4ae441fe356c1127dbd606ad01d229f
Glitch (glitch-hello-eleventy) <none>
2022-08-22 00:39:04 +0000

    ⛵️💎 Checkpoint ./.eleventy.js:43931460/96 ./src/notes/notes.json:43931460/10 ./src/_includes/layouts/base.njk:43931460/10 ./src/_includes/layouts/note.njk:43931460/48 ./src/_includes/layouts/notes.njk:43931460/42 ./src/_includes/layouts/home.njk:43931460/42

--- .eleventy.js
+++ .eleventy.js
@@ -30,20 +30,20 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
 
-  eleventyConfig.addCollection("notes", function(collection) {
+  eleventyConfig.addCollection("posts", function(collection) {
 
     const coll = collection
-      .getFilteredByTag("notes")
+      .getFilteredByTag("posts")
       .sort((a, b) => b.data.date - a.data.date);
 
     // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
     // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
     for (let i = 0; i < coll.length; i++) {
-      const prevNote = coll[i - 1];
-      const nextNote = coll[i + 1];
+      const prevPost = coll[i - 1];
+      const nextPost = coll[i + 1];
 
-      coll[i].data["prevNote"] = prevNote;
-      coll[i].data["nextNote"] = nextNote;
+      coll[i].data["prevPost"] = prevPost;
+      coll[i].data["nextPost"] = nextPost;
     }
 
     return coll;
